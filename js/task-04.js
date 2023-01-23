8c548524372dea3a7a77cef542f7a928a254bf30
Yullia90 <y.tsarenko@harwind.com.ua>
2023-01-23 23:03:59 +0200

    add task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,5 @@
-//Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
+//Лічильник складається зі спану і кнопок, які по кліку повинні
+//збільшувати і зменшувати його значення на одиницю.
 
 //<div id="counter">
 //  <button type="button" data-action="decrement">-1</button>
@@ -6,6 +7,28 @@
 //  <button type="button" data-action="increment">+1</button>
 //</div>
 
-//Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
-//Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+//Створи змінну counterValue, в якій буде зберігатися поточне
+//значення лічильника та ініціалізуй її значенням 0.
+//Додай слухачів кліків до кнопок, всередині яких збільшуй або
+//зменшуй значення лічильника.
 //Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const countercEl = document.querySelector("#counter");
+const btnIncrEl = countercEl.querySelector('[data-action="increment"]');
+const btnDecrEl = countercEl.querySelector('[data-action="decrement"]');
+const spanEl = countercEl.querySelector("#value");
+
+let counterValue = 0;
+console.log(btnIncrEl, btnDecrEl);
+
+function increm() {
+  counterValue += 1;
+  spanEl.textContent = counterValue;
+}
+function decrem() {
+  counterValue -= 1;
+  spanEl.textContent = counterValue;
+}
+
+btnIncrEl.addEventListener("click", increm);
+btnDecrEl.addEventListener("click", decrem);
